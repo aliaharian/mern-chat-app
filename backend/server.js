d3cf79f0e857f833f9e965115045506cc06fe0ae
ali aharian <aliaharian@alis-MacBook-Pro.local>
2023-12-30 09:08:49 +0330

    feat(server.js): add github workflows and edit routes to add frontend to server for production

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
-const protect = require("./middlewares/authMiddleware");
+const path = require("path");
 require("colors");
 
 dotenv.config();
@@ -13,12 +13,25 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.get("/", function (req, res) {
-    res.send("API is running successfully");
-});
+const __dirname1 = path.resolve();
+
 app.use("/api/user", userRoutes);
-app.use(protect).use("/api/chat", chatRoutes);
-app.use(protect).use("/api/message", messageRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "/frontend/dist")));
+
+    app.get("*", (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname1, "frontend", "build", "index.html"),
+        ),
+    );
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running..");
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
