43d8a77de04c738a9fa4692409c64da61ff07892
ali aharian <aliaharian@alis-MacBook-Pro.local>
2023-12-30 12:31:37 +0330

    fix(server.js): exit path of __dirname

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
 
     app.get("*", (req, res) =>
         res.sendFile(
-            path.resolve(__dirname1, "frontend", "build", "index.html"),
+            path.resolve(__dirname1, "frontend", "dist", "index.html"),
         ),
     );
 } else {
